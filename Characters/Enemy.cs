879044a68eebd9bc27a39425455b29b8bf41bfc2
aks-ch <dev@akshat.ch>
2025-10-31 18:07:19 -0400

    Enemy Defined

---
+++ Characters/Enemy.cs
@@ -0,0 +1,91 @@
+﻿using RogueConsoleGame.Enums;
+using RogueConsoleGame.Records;
+using RogueConsoleGame.Screens;
+
+namespace RogueConsoleGame.Characters;
+
+public class Enemy : Character
+{
+    // Enemy Declaration
+    public Enemy(GameScreen game, char symbol, float maxHealth, float strength) : base(game, symbol, maxHealth, strength) {}
+
+    /// <summary>
+    /// Make the enemy move towards a target location.
+    /// </summary>
+    /// <param name="target">The target location to move towards.</param>
+    /// <exception cref="Exception">The target location is inside the enemy.</exception>
+    public void ChooseMove(Vector2 target)
+    {
+        Direction direction = 0;
+        
+        // Determine direction
+        if (target.Y < Position.Y) direction |= Direction.North;
+        if (target.Y > Position.Y) direction |= Direction.South;
+        if (target.X > Position.X) direction |= Direction.East;
+        if (target.X < Position.X) direction |= Direction.West;
+
+        // Invalid direction
+        if (direction == 0) throw new Exception("Enemy's target location is inside itself");
+        
+        bool random = new Random().Next(0, 2) == 1;
+        
+        switch (direction)
+        {
+            // Linear direction
+            case Direction.North:
+                ProcessMove(Position with { Y = Position.Y - 1 });
+                break;
+            case Direction.South:
+                ProcessMove(Position with { Y = Position.Y + 1 });
+                break;
+            case Direction.East:
+                ProcessMove(Position with { X = Position.X + 1 });
+                break;
+            case Direction.West:
+                ProcessMove(Position with { X = Position.X - 1 });
+                break;
+            
+            // Diagonal direction
+            case Direction.North | Direction.East:
+                if (random) ProcessMove(Position with { Y = Position.Y - 1 });
+                else ProcessMove(Position with { X = Position.X + 1 });
+                break;
+            case Direction.South | Direction.East:
+                if (random) ProcessMove(Position with { Y = Position.Y + 1 });
+                else ProcessMove(Position with { X = Position.X + 1 });
+                break;
+            case Direction.South | Direction.West:
+                if (random) ProcessMove(Position with { Y = Position.Y + 1 });
+                else ProcessMove(Position with { X = Position.X - 1 });
+                break;
+            case Direction.North | Direction.West:
+                if (random) ProcessMove(Position with { Y = Position.Y - 1 });
+                else ProcessMove(Position with { X = Position.X - 1 });
+                break;
+        }
+    }
+    
+    /// <summary>
+    /// Process what to do about moving to a new position.
+    /// </summary>
+    /// <param name="newPosition">The new position.</param>
+    private void ProcessMove(Vector2 newPosition)
+    {
+        // new location outside map
+        if (PositionOutOfBounds(newPosition)) return;
+        // enemy in new location
+        if (Game.Enemies.ContainsKey(newPosition)) return;
+
+        // player in new location
+        if (Game.PlayerPosition == newPosition)
+        {
+            if (Game.Player == null) throw new NullReferenceException("The player doesn't exist!");
+            Attack(Game.Player);
+        }
+        // move to location
+        else
+        {
+            Position = newPosition;
+        }
+    }
+}
