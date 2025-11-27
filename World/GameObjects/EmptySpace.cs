b66e7b60f99aff0ac0fdd9f8eb8ca7a02add148d
aks-ch <dev@akshat.ch>
2025-11-27 15:34:19 -0500

    Game Objects, Empty Space

---
+++ World/GameObjects/EmptySpace.cs
@@ -0,0 +1,15 @@
+﻿using RogueConsoleGame.Interfaces;
+
+namespace RogueConsoleGame.World.GameObjects;
+
+public class EmptySpace: IVisible
+{
+    public bool IsVisible { get; private set; }
+    public char Symbol { get; }
+    
+    public EmptySpace(char symbol)
+    {
+        Symbol = symbol;
+    }
+    
+}
