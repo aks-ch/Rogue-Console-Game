5eca8a6fe8a71f74f112f9a082ec0cf3f409f0b0
aks-ch <dev@akshat.ch>
2025-12-02 23:07:41 -0500

    Addition & Subtraction Operators

--- DataTypes/Vector2.cs
+++ DataTypes/Vector2.cs
@@ -1,3 +1,14 @@
 ﻿namespace RogueConsoleGame.DataTypes;
 
-public record Vector2 (int X, int Y);
+public record Vector2(int X, int Y)
+{
+    public static Vector2 operator +(Vector2 v1, Vector2 v2)
+    {
+        return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+    }
+    
+    public static Vector2 operator -(Vector2 v1, Vector2 v2) 
+    {
+        return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+    }
+}
