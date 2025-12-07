9f0e989dd8259b3c81bbac8c325b0422c58054c0
aks-ch <dev@akshat.ch>
2025-12-07 15:07:36 -0500

    Added Item & Key

---
+++ World/GameObjects/Items/Item.cs
@@ -0,0 +1,14 @@
+﻿using RogueConsoleGame.DataTypes;
+
+namespace RogueConsoleGame.World.GameObjects.Items;
+
+/// <summary>
+/// Default constructor for a new item.
+/// </summary>
+/// <param name="map">The map this item is on.</param>
+/// <param name="position">The position of the item on the board.</param>
+/// <param name="hidden">Whether this item is hidden at first.</param>
+public abstract class Item(Map map, Vector2 position, bool hidden) : GameObject(map, position)
+{
+    public bool Hidden { get; set; } = hidden;
+}
---
+++ World/GameObjects/Items/Key.cs
@@ -0,0 +1,14 @@
+﻿using RogueConsoleGame.DataTypes;
+
+namespace RogueConsoleGame.World.GameObjects.Items;
+
+public class Key(Map map, Vector2 position, bool hidden, string keyID) : Item(map, position, hidden)
+{
+    public override char Symbol { get => Hidden ? map.GameManager.EmptyChar : KeySymbol; protected set => KeySymbol = value; }
+    public override ConsoleColor Color { get => Hidden ? ConsoleColor.DarkGray : KeyColor; set => KeyColor = value; }
+    
+    private char KeySymbol { get; set; } = '!';
+    private ConsoleColor KeyColor { get; set; } = ConsoleColor.Yellow;
+
+    public string KeyID { get; } = keyID;
+}
