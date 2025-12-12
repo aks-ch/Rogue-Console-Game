35e619f1f0a98edc5aeab74eea4c4cfb497c3509
aks-ch <dev@akshat.ch>
2025-12-12 15:56:01 -0500

    Fixed hidden item color

--- World/GameObjects/Items/Key.cs
+++ World/GameObjects/Items/Key.cs
@@ -5,7 +5,7 @@ namespace RogueConsoleGame.World.GameObjects.Items;
 public class Key(Map map, Vector2 position, bool hidden, string keyID) : Item(map, position, hidden)
 {
     public override char Symbol { get => Hidden ? Map.GameManager.EmptyChar : KeySymbol; protected set => KeySymbol = value; }
-    public override ConsoleColor Color { get => Hidden ? ConsoleColor.DarkGray : KeyColor; set => KeyColor = value; }
+    public override ConsoleColor Color { get => Hidden ? ConsoleColor.Gray : KeyColor; set => KeyColor = value; }
     
     private char KeySymbol { get; set; } = '!';
     private ConsoleColor KeyColor { get; set; } = ConsoleColor.Yellow;
--- World/GameObjects/Items/WinItem.cs
+++ World/GameObjects/Items/WinItem.cs
@@ -6,7 +6,7 @@ namespace RogueConsoleGame.World.GameObjects.Items;
 public class WinItem(Map map, Vector2 position, bool hidden) : Item(map, position, hidden)
 {
     public override char Symbol { get => Hidden ? Map.GameManager.EmptyChar : WinSymbol; protected set => WinSymbol = value; }
-    public override ConsoleColor Color { get => Hidden ? ConsoleColor.DarkGray : WinColor; set => WinColor = value; }
+    public override ConsoleColor Color { get => Hidden ? ConsoleColor.Gray : WinColor; set => WinColor = value; }
     
     private char WinSymbol { get; set; } = '%';
     private ConsoleColor WinColor { get; set; } = ConsoleColor.Magenta;
